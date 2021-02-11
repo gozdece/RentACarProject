de633e43f8414523c6bc3ee42bb368a6638fd76d
gözde cengiz <gozde.cengiiz98@gmail.com>
2021-02-11 17:27:39 +0300

    Add project files.

---
+++ Business/Concrete/CarManager.cs
@@ -0,0 +1,55 @@
+﻿using Business.Abstract;
+using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Concrete
+{
+    public class CarManager : ICarService
+    {
+        ICarDal _carDal;
+        public CarManager(ICarDal carDal)
+        {
+            _carDal = carDal;
+        }
+        public void Add(Car car)
+        {
+            if (car.DailyPrice>0 && car.CarName.Length>2)
+            {
+                _carDal.Add(car);
+            }
+            else
+            {
+                Console.WriteLine("Arabanın günlük kirası 0 dan ve kelime uzunluğu 2den büyük olmalı");
+            }
+            
+        }
+
+        public void Delete(Car car)
+        {
+            _carDal.Delete(car);
+        }
+
+        public List<Car> GetAll()
+        {
+           return _carDal.GetAll();
+        }
+
+        public List<Car> GetByBrandId(int brandId)
+        {
+            return _carDal.GetAll(x => x.BrandId == brandId);
+        }
+
+        public List<Car> GetByColorId(int colorId)
+        {
+            return _carDal.GetAll(x => x.ColorId == colorId);
+        }
+
+        public void Update(Car car)
+        {
+            _carDal.Update(car);
+        }
+    }
+}
